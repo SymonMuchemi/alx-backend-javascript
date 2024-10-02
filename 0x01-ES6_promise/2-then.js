9a3013d806335c215865cac5a514e2d497f4dcaa
symonmuchemi <simonkenya846@gmail.com>
2024-10-02 20:58:08 +0300

    update: returns immediately

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,5 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch(() => {
-    throw new Error();
-  });
+  }).catch(() => new Error());
 }
