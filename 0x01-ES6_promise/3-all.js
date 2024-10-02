69eb70bccfd1f95811d854d7c3e781bdd065952a
symonmuchemi <simonkenya846@gmail.com>
2024-10-02 21:44:02 +0300

    fix: destructures the result array

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,8 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const { body } = results[0];
-      const { firstName, lastName } = results[1];
-      console.log(`${body} ${firstName} ${lastName}`);
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
