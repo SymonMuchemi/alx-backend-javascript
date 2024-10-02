2accf018d9e8f43b179793c17208172dbc015de9
symonmuchemi <simonkenya846@gmail.com>
2024-10-02 21:14:53 +0300

    fix: eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch(() =>{
+  }).catch(() => {
     console.log('Got a response from the API');
     return new Error();
   }).finally(() => {
