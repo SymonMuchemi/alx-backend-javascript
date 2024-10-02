4d143eb3e231faad26444b1b31573b2d978df3f8
symonmuchemi <simonkenya846@gmail.com>
2024-10-02 20:41:22 +0300

    task: another promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+// returns a promise
+export default function getFullResponseFromApi(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
