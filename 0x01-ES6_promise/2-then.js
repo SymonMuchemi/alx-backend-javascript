189a42b3e1f8e9154a94e693b05c2212ee15bcaf
symonmuchemi <simonkenya846@gmail.com>
2024-10-02 20:54:57 +0300

    update: uses one .then alone

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    console.log('Got a reponse from the API');
-  }).then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
   }).catch(() => {
