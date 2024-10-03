3b3b3a2466694c1fb2bf305f74fafc09d2e3bf7d
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 11:25:29 +0300

    update: return value with value or reason

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,16 @@ import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((results) => results);
+    .then((results) => results.map((result) => {
+        if (result.status === 'fulfilled') {
+            return {
+                status: result.status,
+                value: result.value,
+            };
+        }
+        return{
+            status: result.status,
+            value: result.reason,
+        };
+    }));
 }
