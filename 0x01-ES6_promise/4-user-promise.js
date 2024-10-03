e5984ae3003ff079a25f8ca939dfc5fe8d44e13a
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 09:21:32 +0300

    task: returns a resolved promise with an object

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,6 @@
+// returns a resolved promise with an object
+export default function signUpUser(firstName, lastName) {
+    return new Promise((resolve) => {
+        resolve({ firstName: firstName, lastName: lastName });
+    })
+}
