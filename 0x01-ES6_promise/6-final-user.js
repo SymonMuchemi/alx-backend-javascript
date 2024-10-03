19caa13e29843f9d23c168272e2faa05b6b14968
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 10:56:08 +0300

    update: returns not logs

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,5 @@ import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((results) => console.log(results));
+    .then((results) => results);
 }
