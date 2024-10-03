fed95f40f1cbc96cba8caaa43f7c06abf8e74cda
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 11:45:34 +0300

    test: checks if returning a string message on promise reject will work

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,12 @@
-/* eslint-disable import/extensions */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const res = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    });
-  }
-  return res;
+export default function handleProfileSignup(firstName, lastName, filename) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(filename)
+    ]).then((results) => results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : `Error: ${fileName} cannot be processed`,
+    })));
 }
