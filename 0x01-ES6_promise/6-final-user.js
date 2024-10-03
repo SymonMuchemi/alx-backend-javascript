40a37996d2d4209053f73b8ff3a629fde633d119
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 11:38:06 +0300

    update: return immediately and adds condition on value object

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,11 @@ import signUpUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((results) => results.map((result) => {
-        if (result.status === 'fulfilled') {
-            return {
-                status: result.status,
-                value: result.value,
-            };
-        }
-        return{
-            status: result.status,
-            value: result.reason,
-        };
-    }));
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(filename)
+    ]).then((results) => results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
