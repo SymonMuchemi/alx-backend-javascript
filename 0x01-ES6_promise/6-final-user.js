feba603883793a9f3da35939acf76dbff5c109c2
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 10:53:55 +0300

    task: returns results of two promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((results) => console.log(results));
+}
