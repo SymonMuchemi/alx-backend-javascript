9481d4a074883150baa3b1f37d1e77d0e4babdb7
symonmuchemi <simonkenya846@gmail.com>
2024-10-06 01:57:33 +0300

    task: calls functions and waits for response

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+// calls uploadPhoto and createUser and returns an object
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => ({
+      photo: { photoData },
+      user: { userData },
+    })).catch(() => ({
+      photo: null,
+      user: null,
+    }));
+}
