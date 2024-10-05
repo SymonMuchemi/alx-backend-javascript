57ef01b567342001b709f1b260f555eac7175d04
symonmuchemi <simonkenya846@gmail.com>
2024-10-05 12:07:25 +0300

    task: load balancer

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,4 @@
+// return the first promise to resolve
+export default async function loadBalancer(chinaDownload, USDownload) {
+    return Promise.race([chinaDownload, USDownload]).then(result => result);
+}
