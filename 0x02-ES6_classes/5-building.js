0ad93013919d7c0d2d81a6c6d63d6ed1a1c54e10
symonmuchemi <simonkenya846@gmail.com>
2024-10-11 09:02:29 +0300

    task: attempts to create an abstract class with an abstract method

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+export default class Building {
+  constructor(sqft) {
+    Building.checkSqft(sqft);
+    this._sqft = sqft;
+  }
+
+  static checkSqft(x) {
+    if (typeof x !== 'number') throw TypeError('Sqft must be a number!');
+  }
+
+  // getter
+  get sqft() {
+    return this._sqft;
+  }
+
+  // getter
+  set sqft(x) {
+    Building.checkSqft(x);
+    this._sqft = x;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
