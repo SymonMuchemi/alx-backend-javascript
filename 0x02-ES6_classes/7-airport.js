d36813e17203832c476dda8c387f5dc2fa4acbe7
symonmuchemi <simonkenya846@gmail.com>
2024-10-11 10:21:49 +0300

    task: attempts to override the toString() method

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+export default class Airport {
+  constructor(name, code) {
+    Airport.checkName(name);
+    Airport.checkCode(code);
+    this._name = name;
+    this._code = code;
+  }
+
+  static checkName(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string!');
+  }
+
+  static checkCode(code) {
+    if (typeof code !== 'string') throw TypeError('Code must be a string!');
+  }
+
+  toString() {
+    return `${this._code}`;
+  }
+}
