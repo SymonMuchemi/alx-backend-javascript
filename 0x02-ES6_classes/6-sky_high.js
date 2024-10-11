7df724164aca85de17b5546605deba90c19dcb3e
symonmuchemi <simonkenya846@gmail.com>
2024-10-11 09:35:33 +0300

    task: creates SkyHighBuilding class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,29 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    SkyHighBuilding.checkFloors(floors);
+    this._floors = floors;
+  }
+
+  static checkFloors(floors) {
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be a number!');
+    }
+  }
+
+  // floors getter
+  get floors() {
+    return this._floors;
+  }
+
+  // sqft getter
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
