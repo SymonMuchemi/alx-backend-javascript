038a89fb48bc3676700893c62734a4c08cddac9b
symonmuchemi <simonkenya846@gmail.com>
2024-11-24 21:13:12 +0300

    task: filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(studentList, location) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  return studentList.filter(((student) => student.location === location));
+}
