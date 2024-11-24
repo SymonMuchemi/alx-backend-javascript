15683200a4361e15ff9ca52764c9539b61eab4ae
symonmuchemi <simonkenya846@gmail.com>
2024-11-24 12:18:56 +0300

    fix: remove parameter requirements

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,9 +1,5 @@
 export default class Car {
   constructor(brand, motor, color) {
-    Car.checkBrand(brand);
-    Car.checkMotor(motor);
-    Car.checkColor(color);
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
@@ -13,23 +9,5 @@ export default class Car {
     const ClonedCar = this.constructor;
 
     return new ClonedCar(this._brand, this._motor, this._color);
-  }
-
-  static checkBrand(brand) {
-    if (typeof brand !== 'string') {
-      throw TypeError('brand must be a string');
-    }
-  }
-
-  static checkMotor(motor) {
-    if (typeof motor !== 'string') {
-      throw TypeError('motor must be a string');
-    }
-  }
-
-  static checkColor(color) {
-    if (typeof color !== 'string') {
-      throw TypeError('color must be a string');
-    }
   }
 }
