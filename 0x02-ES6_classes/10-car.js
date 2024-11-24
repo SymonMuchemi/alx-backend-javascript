b69c7d988f8fa573045fe4fed9676e5fe2f34bd1
symonmuchemi <simonkenya846@gmail.com>
2024-11-24 12:15:31 +0300

    fix: solve lint issues

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,36 +1,35 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        Car.checkBrand(brand);
-        Car.checkMotor(motor);
-        Car.checkColor(color);
+  constructor(brand, motor, color) {
+    Car.checkBrand(brand);
+    Car.checkMotor(motor);
+    Car.checkColor(color);
 
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const ClonedCar = this.constructor;
+
+    return new ClonedCar(this._brand, this._motor, this._color);
+  }
+
+  static checkBrand(brand) {
+    if (typeof brand !== 'string') {
+      throw TypeError('brand must be a string');
     }
+  }
 
-
-    cloneCar() {
-        const clonedCar = this.constructor;
-
-        return new clonedCar(this._brand, this._motor, this._color)
+  static checkMotor(motor) {
+    if (typeof motor !== 'string') {
+      throw TypeError('motor must be a string');
     }
+  }
 
-    static checkBrand(brand) {
-        if (typeof brand !== 'string') {
-            throw TypeError('brand must be a string')
-        }
+  static checkColor(color) {
+    if (typeof color !== 'string') {
+      throw TypeError('color must be a string');
     }
-
-    static checkMotor(motor) {
-        if (typeof motor !== 'string') {
-            throw TypeError('motor must be a string')
-        }
-    }
-
-    static checkColor(color) {
-        if (typeof color !== 'string') {
-            throw TypeError('color must be a string')
-        }
-    }
+  }
 }
