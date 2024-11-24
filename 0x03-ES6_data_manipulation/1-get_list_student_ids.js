d8f478633cf490e2ecc7c321500b2e2ed90dd446
symonmuchemi <simonkenya846@gmail.com>
2024-11-24 21:02:09 +0300

    uses arr.map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,7 @@
 export default function getListStudentIds(objArray) {
-  const map = new Map();
-
   if (!Array.isArray(objArray)) {
     return [];
   }
 
-  for (let i = 0; i < objArray.length; i += 1) {
-    map.set(i, objArray[i].id);
-  }
-
-  return Array.from(map.values());
+  return objArray.map(((obj) => obj.id));
 }
