9f1a1d158ca487b6a199a3ea3eb649f1366a4f9e
symonmuchemi <simonkenya846@gmail.com>
2024-11-27 11:30:51 +0300

    fix: adds a trycatch block to gracefull hanlde err

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -7,7 +7,6 @@ const PORT = 1245;
 async function readCSVFile(filepath) {
   try {
     const data = await fs.readFile(filepath, 'utf-8');
-
     return data;
   } catch (error) {
     throw new Error('Cannot load the database');
@@ -15,36 +14,36 @@ async function readCSVFile(filepath) {
 }
 
 async function countStudents(database) {
-  const data = await readCSVFile(database);
+  try {
+    const data = await readCSVFile(database);
 
-  if (!data) {
-    throw new Error('Cannot load database');
+    let responseStr = '';
+
+    const arr = data.split('\n');
+
+    arr.shift();
+    arr.pop();
+
+    responseStr += `Number of students: ${arr.length}\n`;
+
+    // get fields
+    const fields = arr.map(((e) => e.split(',')[3]));
+    const uniqueFields = new Set(fields);
+    const fieldArr = [...uniqueFields];
+
+    // log students in each field
+    for (const field of fieldArr) {
+      const lst = arr.filter(((e) => e.split(',')[3] === field));
+      const names = lst.map(((e) => e.split(',')[0]));
+      const numberOfStudents = lst.length;
+
+      responseStr += `Number of students in ${field}: ${numberOfStudents}. List: ${names.join(', ')}\n`;
+    }
+
+    return responseStr;
+  } catch (error) {
+    return 'Cannot load the database';
   }
-
-  let responseStr = '';
-
-  const arr = data.split('\n');
-
-  arr.shift();
-  arr.pop();
-
-  responseStr += `Number of students: ${arr.length}\n`;
-
-  // get fields
-  const fields = arr.map(((e) => e.split(',')[3]));
-  const uniqueFields = new Set(fields);
-  const fieldArr = [...uniqueFields];
-
-  // log students in each field
-  for (const field of fieldArr) {
-    const lst = arr.filter(((e) => e.split(',')[3] === field));
-    const names = lst.map(((e) => e.split(',')[0]));
-    const numberOfStudents = lst.length;
-
-    responseStr += `Number of students in ${field}: ${numberOfStudents}. List: ${names.join(', ')}\n`;
-  }
-
-  return responseStr;
 }
 
 app.get('/', (req, res) => {
