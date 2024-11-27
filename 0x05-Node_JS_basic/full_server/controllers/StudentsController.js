5df30390694fb238e8a1a7ccfd63dcce98dd28fc
symonmuchemi <simonkenya846@gmail.com>
2024-11-27 13:56:33 +0300

    feat: creates StudentController.getAllStudents

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,11 +1,33 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static getAllStudents(request, response) {
+  static async getAllStudents(request, response) {
+    try {
+      const lastCommandLineArgumentIndex = process.argv.length - 1;
+      const studentsData = await readDatabase(
+        process.argv[lastCommandLineArgumentIndex],
+      );
 
-    const studentsData = await readDatabase()
+      if (studentsData.length === 0) {
+        return response.status(500).send('Cannot load database');
+      }
 
-    return response.status(200).send('This is the list of students');
+      let message = 'This is the list of students';
+
+      const fields = Object.keys(studentsData).sort((a, b) => a - b);
+
+      for (const field of fields) {
+        const list = studentsData.get(field);
+        console.log(list);
+
+        message += `\nNumber of students in ${field}: ${
+          list.len
+        }. List: ${list.join(', ')}`;
+      }
+      return response.status(200).send(message);
+    } catch (error) {
+      return response.status(500).send('Cannot load database');
+    }
   }
 }
 
