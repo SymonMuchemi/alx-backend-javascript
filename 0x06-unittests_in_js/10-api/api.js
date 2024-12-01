024ce5eeefcf5d71c3b7529b49f9bebb83a760b1
symonmuchemi <simonkenya846@gmail.com>
2024-12-01 22:56:56 +0300

    task: deep equality and post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.use(express.json()); // Middleware to parse JSON
+
+const paymentObj = {
+  payment_methods: {
+    credit_cards: true,
+    paypal: false,
+  },
+};
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (!Number.isNaN(id)) {
+    return res.status(200).send(`Payment methods for cart ${id}`);
+  }
+  return res.status(404).send(':id must be a number');
+});
+
+app.get('/available_payments', (req, res) => {
+  res.status(200).json(paymentObj);
+});
+
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+
+  try {
+    if (!username || username.length === 0) {
+      return res.status(400).send('Invalid username');
+    }
+    return res.status(200).send(`Welcome ${username}`);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,97 @@
+/* eslint-disable quotes */
+/* eslint-disable jest/no-test-callback */
+/* eslint-disable jest/lowercase-name */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+const request = require("request");
+const { expect } = require("chai");
+
+const API_URL = "http://localhost:7865";
+
+describe("API integration test", () => {
+  const paymentObj = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+
+  it("GET / returns correct response", (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.body).to.be.equal("Welcome to the payment system");
+      done();
+    });
+  });
+
+  it("GET /cart/:id returns the correct response", (done) => {
+    request.get(`${API_URL}/cart/23`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal("Payment methods for cart 23");
+      done();
+    });
+  });
+
+  it("GET /cart/:id returns the correct response on invalid id", (done) => {
+    request.get(`${API_URL}/cart/p`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      expect(body).to.be.equal(":id must be a number");
+      done();
+    });
+  });
+
+  it("GET /available_payments returns correct response", (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body)).to.deep.equal(paymentObj);
+      done();
+    });
+  });
+});
+
+describe("POST /login", () => {
+  it("POST /login with valid username returns correct response", (done) => {
+    request.post(
+      {
+        url: `${API_URL}/login`,
+        body: { username: "JohnDoe" },
+        json: true,
+      },
+      (_err, res, body) => {
+        if (res) {
+          expect(res.statusCode).to.be.equal(200);
+          expect(body).to.be.equal("Welcome JohnDoe");
+        } else {
+          console.error('Request failed:', _err.message);
+        }
+        done();
+      },
+    );
+  });
+
+  it("POST /login with missing username returns 400", (done) => {
+    request.post(
+      {
+        url: `${API_URL}/login`,
+        json: {},
+      },
+      (_err, res, body) => {
+        expect(res.statusCode).to.be.equal(400);
+        done();
+      },
+    );
+  });
+
+  it("POST /login with invalid JSON returns 400", (done) => {
+    request.post(
+      {
+        url: `${API_URL}/login`,
+        body: "invalid_json",
+      },
+      (_err, res, body) => {
+        expect(res.statusCode).to.be.equal(400);
+        done();
+      },
+    );
+  });
+});
