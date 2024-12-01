22cc758b8ea8cc3b67505e7ad83e5776d3536a7a
symonmuchemi <simonkenya846@gmail.com>
2024-12-01 19:52:58 +0300

    update: use .round instead of .ceil

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
 const calculateNumber = (a, b) => {
-  const result = Math.ceil(a) + Math.ceil(b);
+  const result = Math.round(a) + Math.round(b);
 
-  return Object.is(result, -0)? 0 : result;
-}
+  return Object.is(result, -0) ? 0 : result;
+};
 
-module.exports = calculateNumber
+module.exports = calculateNumber;
